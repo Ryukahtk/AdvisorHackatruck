b7fd4a48970ad338b711966d38174936e608e249
student <student@student02.local>
2019-07-02 17:54:09 -0300

    primeira att

---
+++ AdvisorHackatruck/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AdvisorHackatruck
+//
+//  Created by student on 02/07/19.
+//  Copyright © 2019 student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
