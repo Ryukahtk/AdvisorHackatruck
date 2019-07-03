6a9d19a0862a9c59f487504b7af354db05546ea7
ryukahtk <ryukahtk@ryukahtks-Mac.local>
2019-07-03 09:35:14 -0700

    Projeto inicial

--- AdvisorHackatruck/ViewController.swift
+++ AdvisorHackatruck/ViewController.swift
@@ -8,11 +8,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+  
+    @IBOutlet weak var tblPasta: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        tblPasta.dataSource = self
+        tblPasta.delegate = self
+    }
+    
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+
+        let cell = tableView.dequeueReusableCell(withIdentifier: "pastaid")
+        return cell!
+        
+    }
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath){
+        tableView.deselectRow(at: indexPath, animated: true)
     }
 
     override func didReceiveMemoryWarning() {
